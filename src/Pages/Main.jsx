eeb4fb2cef152f6c82f9b5e3695cfefe6e530581
Kseniya Avdeenko <105524305+KseniyaAvdeenko@users.noreply.github.com>
2024-06-20 18:55:40 +0300

    main page

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import mainStyles from '../assets/styles/main.module.sass'
+import MainImg from '../assets/images/mainImg.png'
 
 const Main = () => {
     return (
         <main className={mainStyles.main}>
-            main page
+            <div className={mainStyles.main__imageWrapper}>
+                <img src={MainImg} alt="image"/>
+            </div>
+            <h1 className={mainStyles.main__heading}>Master the chaos</h1>
+            <h4 className={mainStyles.main__subheading}>get inspired, create, explore</h4>
         </main>
     );
 };
